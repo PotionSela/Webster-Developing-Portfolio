437703d57b17d7ba8cb6f42c316d735c289e9da8
Gisela <giselamata27@gmail.com>
2024-03-08 15:47:35 -0500

    Added placeholder code for navi

---
+++ web-developer-portfolio/src/components/Navigation/Nav.jsx
@@ -0,0 +1,41 @@
+import { NavLink } from "react-router-dom";
+import "./nav.css";
+
+export default function Navigation() {
+  return (
+    <ul className="nav nav-pills navbar">
+      <li className="nav-item">
+        <NavLink
+          to="/"
+          className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+        >
+          ABOUT ME
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="/portfolio"
+          className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+        >
+          PORTFOLIO
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="/contact"
+          className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+        >
+          CONTACT
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="/resume"
+          className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+        >
+          RESUME
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
