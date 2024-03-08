8e166c625a0937ce4be78c4e86c31b4865c9d521
Gisela <giselamata27@gmail.com>
2024-03-08 15:24:29 -0500

    Added imports and children

--- web-developer-portfolio/src/main.jsx
+++ web-developer-portfolio/src/main.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App.jsx'
+import About from './components/pages/About.jsx'
+import Contact from './components/pages/Contact.jsx'
+import Portfolio from './components/pages/Portfolio.jsx'
+import Resume from './components/pages/Resume.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        index: true,
+        element: <Contact />,
+      },
+      {
+        index: true,
+        element: <Portfolio />,
+      },
+      {
+        index: true,
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
