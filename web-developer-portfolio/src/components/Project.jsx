7ea078f1e68e9e9976590b4c551c0ef9d5d82784
Gisela <giselamata27@gmail.com>
2024-03-08 17:24:46 -0500

    Added media screens to nav, added placeholder for Project file with the CSS included

---
+++ web-developer-portfolio/src/components/Project.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import "./project.css";
+import githublogoWhite from '../assets/GitHub_Logo_White.png'
+
+export default function Project(props) {
+  return (
+    <div className="card">
+      <a href={props.url} target="_blank" rel="noreferrer">
+        <img className="projectImage" src={props.img} alt={props.alt}></img>
+      </a>
+      <h3>{props.title}</h3>
+      <p>{props.desc}</p>
+      <a href={props.repo} target="_blank" rel="noreferrer">
+        <img className="githubLogo" src={githublogoWhite} alt="GitHub Project Repository"></img>
+      </a>
+    </div>
+  );
+}
