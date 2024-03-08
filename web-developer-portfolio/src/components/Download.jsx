7c54e2a59b605737cd3f7ca21b4d83dfb4406b03
Gisela <giselamata27@gmail.com>
2024-03-08 17:10:28 -0500

    Adding in, hopefully, the resume, and styling

--- web-developer-portfolio/src/components/Download.jsx
+++ web-developer-portfolio/src/components/Download.jsx
@@ -1,1 +1,16 @@
-import "."
+import "./download.css";
+import resume from "../assets/resume1.png";
+import resume2 from "../assets/resume2.png";
+
+export default function Download() {
+  return (
+    <div className="container">
+      <a className="btn" href={resume} download="resume1.png">
+        Download the front side of my Resume!
+      </a>
+      <a className="btn" href={resume2} download="resume2.png">
+        Download the back side of my Resume!
+      </a>
+    </div>
+  );
+}
